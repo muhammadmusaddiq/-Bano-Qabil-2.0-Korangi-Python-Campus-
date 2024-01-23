3d3926b4b60cc84c052e487eee5ab25905d5dd58
muhammadmusaddiq <157229253+muhammadmusaddiq@users.noreply.github.com>
2024-01-23 20:41:41 +0500

    Create BanoQabilCalculator.py

---
+++ BanoQabilCalculator.py
@@ -0,0 +1,28 @@
+print("BANO QABIL CALCULATOR:")
+print("1-Add")
+print("2-Subtract")
+print("3-Multiplication")
+print("4-Divide")
+option = int(input("Choose an operation:"))
+
+if(option in [1,2,3,4]):
+    num1 = int(input("Enter first number:"))
+    num2 = int(input("Enter second number:"))
+    
+    if(option == 1):
+        result = num1 + num2
+    elif(option == 2):
+        result = num1 - num2
+    elif(option == 3):
+        result = num1 * num2
+    elif(option == 4):
+        result = num1 // num2
+        
+    
+else:
+    print("Invalid operation Entered")
+    
+    
+print("The result of this operation is {}" .format(result))
+    
+    
